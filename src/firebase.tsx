b02382f998b6cf4c86447f9047126668e785167b
Danielkakona <Daniel@bridgelabs.design>
2020-08-29 14:27:20 +0200

    Header component

---
+++ src/firebase.tsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYQHNeagNcxzkErURGAZsdnTIe00vTxMY",
+  authDomain: "facebook-clone-91564.firebaseapp.com",
+  databaseURL: "https://facebook-clone-91564.firebaseio.com",
+  projectId: "facebook-clone-91564",
+  storageBucket: "facebook-clone-91564.appspot.com",
+  messagingSenderId: "1029640174914",
+  appId: "1:1029640174914:web:0d64dac31a07f20e69dcdb",
+  measurementId: "G-8SPJZ5SQJP"
+};
